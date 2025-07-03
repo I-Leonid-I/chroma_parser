86c7362d3454490d48ac7cd1f335afbae7f2fb18
Leonid <104693024+I-Leonid-I@users.noreply.github.com>
2025-07-03 04:06:27 +0300

    Sample string parsed

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,19 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified MyLib
+import Text.Megaparsec
+import Data.Void
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  let input = "Hello World"
+  let res :: Either (ParseErrorBundle String Void) String
+      -- runParser - to run the parser
+      -- parser function name
+      -- source name (file name)
+      -- input string to parse
+      res = runParser MyLib.parserString "" input
+  case res of
+    Left err -> putStrLn $ "Parse error: " ++ show err
+    Right value -> putStrLn $ "Parsed: " ++ value
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,9 +1,15 @@
-module MyLib (someFunc) where
+module MyLib where
 
 import Text.Megaparsec
+import Text.Megaparsec.Char
 import Data.Void
 
 type Parser = Parsec Void String
+
+parserString :: Parser String
+parserString = do
+    a <- string "Hello"
+    return a
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
